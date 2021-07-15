2f21da9d24a23c3cd1ae77e50cff4882e9b1d039
qzig <q-zig@live.fr>
2021-07-15 13:25:35 +0200

    fix bg button

--- components/home/skillbar.vue
+++ components/home/skillbar.vue
@@ -3,7 +3,7 @@
     <p class="my-3 font-bold">{{label}}</p>
     <div class="w-full h-4 bg-gray-400 rounded-lg">
       <div :class="`h-full rounded-lg flex justify-end items-center ${color}`" :style="`width: ${value}%`">
-        <div :class="`rounded-full h-12 w-12 ${color}`"></div>
+        <div :class="`rounded-full h-12 w-12 ${buttonColor}`"></div>
       </div>
     </div>
   </div>
